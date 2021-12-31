22b53ecb6ad819d4f35f20257ca1a3a1ac1120b8
ailurid <23497994+ailurid@users.noreply.github.com>
2021-12-31 11:21:24 +0800

    add variable for size of square game container

--- index.ts
+++ index.ts
@@ -4,10 +4,12 @@ import { Preloader } from './src/scene/Preloader';
 import { Start } from './src/scene/Start';
 import { Runner } from './src/scene/Runner';
 
+const square_size: number = 320
+
 const runnerConfig: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: 320, // 1334
-  height: 320, // 750
+  width: square_size,
+  height: square_size,
   scene: [Preloader, Start, Runner],
   physics: {
     default: 'arcade',
